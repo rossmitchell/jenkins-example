46183eb94f2ac62302a0c9007fa572601aab8046
Ross Mitchell <ross@edmondscommerce.co.uk>
2018-03-02 16:51:31 +0000

    Lets get magento 2 running on this

--- runTest.sh
+++ runTest.sh
@@ -14,9 +14,28 @@ $(hostname) $0 $@
 
 webDirectory="/var/www/vhosts/magento2/"
 
-cp -r ${DIR}/. ${webDirectory}
+composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition ${DIR}/magento
+
+cp -r ${DIR}/magento/. ${webDirectory}
 
 cd ${webDirectory}
+
+php ${webDirectory}/bin/magento setup:install \
+ --base-url="http://www.magento2.dev/" \
+ --db-host="localhost" \
+ --db-name="magento2" \
+ --db-user="magento2" \
+ --db-password="magento2" \
+ --admin-firstname="admin" \
+ --admin-lastname="user" \
+ --admin-email="test@example.com" \
+ --admin-user="admin" \
+ --admin-password="admin123" \
+ --language="en_US" \
+ --currency="USD" \
+ --timezone="Europe/London" \
+ --use-rewrites=0 \
+ --backend-frontname=admin
 
 ls -lah
 
